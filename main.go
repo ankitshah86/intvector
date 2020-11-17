c43a8ad515f1f6aea94dff48ad1c43fd28827329
ankitshah86 <ankit.r.shah63@gmail.com>
2020-11-16 21:33:34 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package intvector
+
+import (
+	"runtime"
+)
+
+//Intvector is a vector implementation in golang
+type Intvector struct {
+	vec []int
+}
+
+//Push inserts/pushes a new integer into the int slice
+func (v *Intvector) Push(s int) {
+	v.vec = append(v.vec, s)
+}
+
+//Pop removes the first element from the slice and retruns it
+func (v *Intvector) Pop() int {
+	var s int
+
+	if len(v.vec) > 0 {
+		s = v.vec[len(v.vec)-1]
+		v.vec = v.vec[:len(v.vec)-1]
+	} else {
+		//add better handling here
+		return 0
+	}
+
+	return s
+}
+
+//Size returns the current size of the vector
+func (v *Intvector) Size() int {
+	return len(v.vec)
+}
+
+//Clear clears out the slice and invokes the garbage collector to reclaim the freed memory.
+func (v *Intvector) Clear() {
+	v.vec = nil
+	runtime.GC()
+}
