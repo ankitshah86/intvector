77e958497b7ed7844730a5ee25b2ec716ad3bc94
ankitshah86 <ankit.r.shah63@gmail.com>
2020-12-08 09:45:39 -0500

    added test for insert method

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package intvector
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPush(t *testing.T) {
 	var v Intvector
@@ -13,5 +15,27 @@ func TestPush(t *testing.T) {
 
 	if !(newLength == curLength+1 && v.vec[len(v.vec)-1] == 4) {
 		t.Errorf("Push Test failed")
+	}
+}
+
+func TestInsert(t *testing.T) {
+
+	var s Intvector
+
+	sliceToInsert := []int{1, 2, 3}
+	vecLength := len(s.vec)
+
+	s.Insert(sliceToInsert...)
+	newVecLength := len(s.vec)
+
+	if newVecLength-vecLength != len(sliceToInsert) {
+		t.Errorf("Insert Length Test failed")
+	}
+
+	//the last elements must be th ones inserted
+	for i := newVecLength - 1; i > vecLength-1; i-- {
+		if s.vec[i] != sliceToInsert[i-vecLength] {
+			t.Errorf("Insert element Test failed`")
+		}
 	}
 }
