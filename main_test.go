4791ccce7c467b067e3f3b31f84e8a5a43519568
ankitshah86 <ankit.r.shah63@gmail.com>
2020-12-10 20:20:25 -0500

    added benchmark for Push method

--- main_test.go
+++ main_test.go
@@ -18,6 +18,15 @@ func TestPush(t *testing.T) {
 	}
 }
 
+func BenchmarkPush(b *testing.B) {
+	var s Intvector
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 1; j++ {
+			s.Push(j)
+		}
+	}
+}
+
 func TestInsert(t *testing.T) {
 
 	var s Intvector
