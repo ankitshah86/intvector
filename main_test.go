21eae740a2445205dec3a40c9dd5577fb4557dcd
ankitshah86 <ankit.r.shah63@gmail.com>
2020-12-25 09:58:50 -0500

    Added test for First method

--- main_test.go
+++ main_test.go
@@ -409,3 +409,34 @@ func TestIsSorted(t *testing.T) {
 		t.Errorf("IsSorted Test failed : want %t got %t", want, got)
 	}
 }
+
+func TestFirst(t *testing.T) {
+	var s Intvector
+
+	res, err := s.First()
+
+	want := 0
+	got := res
+
+	if err == nil {
+		t.Errorf("First method Test failed : should throw error for empty vector")
+	}
+
+	if want != got {
+		t.Errorf("First method Test failed : want %d, got %d", want, got)
+	}
+
+	s.Insert([]int{1, 2, 3, 4, 5}...)
+
+	want = 1
+	got, err = s.First()
+
+	if err != nil {
+		t.Errorf("First method Test failed : should not throw error for empty vector")
+	}
+
+	if want != got {
+		t.Errorf("First method Test failed : want %d, got %d", want, got)
+	}
+
+}
